e34d4d38b9f0b3bf64cdee4f7f95d91952ce9c7f
onmedicines <semwalanurag26@gmail.com>
2024-10-04 12:40:57 +0530

    Retouches in intro

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -4,7 +4,6 @@ export default function Introduction() {
       <header>
         <span>1</span>
         <h1>Intoduction</h1>
-        <p>A brief introduction about yourself within 3-4 lines.</p>
       </header>
       <main>
         <textarea rows={4} name="intro" placeholder="Highly creative and multi-talented Web Designer... "></textarea>
